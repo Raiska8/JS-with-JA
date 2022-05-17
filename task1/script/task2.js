5bddd84375d26d17d59123af455cf1329df8ffe3
Raisa Abdrai kyzy <raisa.abdraikyzy.kk@gmail.com>
2022-05-18 00:29:55 +0300

    docs: task1 folder added

---
+++ task1/script/task2.js
@@ -0,0 +1,8 @@
+/*Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение*/
+
+let task2 = 'Hello World!';
+console.log(task2);
+
+task2 = `let's learn JavaScript!`;
+console.log(task2);
+document.write(task2)
---
+++ task1/script/task3.js
@@ -0,0 +1,4 @@
+//Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+let userName = "Katie";
+    userEmail = "katie@gmail.com";
+    console.log(userName, userEmail);

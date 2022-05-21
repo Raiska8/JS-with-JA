a5b0ecb56a9340cff5f58f6a1bee82e38bc4dbef
Raisa Abdrai kyzy <raisa.abdraikyzy.kk@gmail.com>
2022-05-21 21:07:49 +0300

    feat: task 1 fixed and updated

--- task1/script/task4.js
+++ task1/script/task4.js
@@ -1,5 +1,13 @@
 /*Интернет-магазин занимается продажей различных сувениров и безделушек. Каждый сувенир весит 75 г, а безделушка – 112 г. Напишите программу, запрашивающую у пользователя количество тех и других покупок, после чего выведите на экран общий вес посылки.*/
-let souvenir = 75;
-let accessories = 122;
-console.log(souvenir + accessories);
-document.write(`Общий вес посылки: ${souvenir + accessories}`+ " г." + "<br>" );
+const souvenirWeight = 75;
+      accessoriesWeight = 122;
+
+
+let souvenirAmount = prompt("Cколько сувениров купили?"),
+    accessoriesAmount = prompt("Cколько безделушек купили?");
+
+let sum = ((souvenirWeight * souvenirAmount) + (accessoriesWeight * accessoriesAmount));
+alert("Oбщий вес посылки " + sum + "г" );
+//document.write(`Oбщий вес посылки ${sum}г`);
+document.write(`Общий вес посылки: ${sum}`+ " г." + "<br>" );
+console.log(`Oбщий вес посылки ${sum}г`);
--- task1/script/task5.js
+++ task1/script/task5.js
@@ -12,7 +12,7 @@
     
     - результат возведения числа a в степень b*/
 
-    let a = 30;
+    /*let a = 30;
     let b = 3
     console.log(a + b); // +
     document.write(`сумма a и b: ${a + b}` + "<br>");
@@ -30,7 +30,25 @@
     document.write(`остаток от деления a на b: ${a % b}`+"<br>");
 
     console.log(a ** b); // **
-    document.write(`результат возведения числа a в степень b* : ${a ** b}`+"<br>");
+    document.write(`результат возведения числа a в степень b* : ${a ** b}`+"<br>");*/
+
+    let a = prompt("Выведите целое число?");
+    a = Number(a);
+    let b = prompt("Выведите целое число?");
+    b = Number(b);
+    document.write("сумма a и b = " + (a + b) + "<br>");
+    document.write("разница между a и b = " + (a - b) + "<br>");
+    document.write("произведение a и b = " + (a * b) + "<br>");
+    document.write("частное от деления a на b = " + (a / b) + "<br>");
+    document.write("остаток от деления a на b = " + (a % b) + "<br>");
+    document.write("результат возведения числа a в степень b = " + (a ** b));
+
+    console.log(`сумма a и b = ${a + b}`);
+    console.log(`разница между a и b = ${a - b}`);
+    console.log(`произведение a и b = ${a * b}`);
+    console.log(`частное от деления a на b = ${a / b}`);
+    console.log(`остаток от деления a на b = ${a % b}`);
+    console.log(`результат возведения числа a в степень b = ${a ** b}`);
 
    
 
